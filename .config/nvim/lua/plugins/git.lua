2e2b57a3172776d80c49d836193b42f30ab55bb7
pjinhispjs <git@pjmorley.com>
2024-08-24 19:01:16 -0400

    Removed now depricated group name sheme

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -4,7 +4,6 @@ return {
 		config = function ()
 			require('gitsigns').setup()
 			local wk = require("which-key")
-			wk.register({g = { name = "Git"}})
 			vim.api.nvim_set_keymap("", "<leader>gh", "<cmd>Gitsigns preview_hunk<cr>", {noremap = true, desc = "Show hunk"})
 			vim.api.nvim_set_keymap("", "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", {noremap = true, desc = "Reset hunk"})
 			vim.api.nvim_set_keymap("", "<leader>gb", "<cmd>Gitsigns blame_line<cr>", {noremap = true, desc = "Git blame"})
