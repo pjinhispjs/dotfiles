fc502470983c646f06e5bf506ff7479fcb8ced5c
pjinhispjs <git@pjmorley.com>
2024-08-25 00:01:19 -0400

    Converted lsp to setup from josean

--- .config/nvim/lua/plugin_manager.lua
+++ .config/nvim/lua/plugin_manager.lua
@@ -1,4 +1,3 @@
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -13,7 +12,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Load plugins listed in .config/nvim/lua/plugins.lua
-require("lazy").setup("plugins")
+-- Load the lsp plugins from a separate folder.  I suspect they get loaded after
+-- the inital set of plugins.  For some reason this is required or the LSP never
+-- runs.
+require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } })
 
 vim.api.nvim_set_keymap("", "<leader>l", "<Cmd>Lazy<CR>", {noremap = true, silent = false, expr = false})
-
