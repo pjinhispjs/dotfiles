7f0ad4c024d9ba15fb44024ef4108cbd71b7a8e6
pjinhispjs <git@pjmorley.com>
2024-08-25 00:21:51 -0400

    Added clangd to mason.  Reordered the language list.

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -27,12 +27,13 @@ return {
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
-				"html",
+				"clangd",
 				"cssls",
-				"tailwindcss",
-				"svelte",
+				"html",
 				"lua_ls",
 				"pylsp",
+				"svelte",
+				"tailwindcss",
 			},
 		})
 
