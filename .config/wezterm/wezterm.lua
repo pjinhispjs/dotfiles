e520f54fbc97fe974ad23d72cdb458ee444f907f
pjinhispjs <git@pjmorley.com>
2024-11-05 13:06:43 -0500

    Weztern config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Everforest Dark (Gogh)'
+config.font = wezterm.font_with_fallback {'Fira Code', 'Victor Mono',}
+
+return config
