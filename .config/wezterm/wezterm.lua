96dd218b5b31561cf3e407bf5dffece417ca2461
pjinhispjs <git@pjmorley.com>
2024-11-11 20:50:40 -0500

    Fixed whitespace.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -27,10 +27,10 @@ config.window_decorations = "NONE"
 config.hide_tab_bar_if_only_one_tab = true
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 -- Key bindings
