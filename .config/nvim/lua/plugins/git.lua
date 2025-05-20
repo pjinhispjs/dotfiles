332e5372a3b8b8bcfd2b672d47ce059ca6d1c435
pjinhispjs <git@pjmorley.com>
2025-05-20 14:38:49 -0400

    Add vim-fugitive

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,12 +1,34 @@
 -- https://github.com/lewis6991/gitsigns.nvim
 return {
-	{ "lewis6991/gitsigns.nvim",
-		config = function ()
-			require('gitsigns').setup()
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.api.nvim_set_keymap("", "<leader>gg", "<cmd>Git<cr>", { noremap = true, desc = "Git status" })
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
 			local wk = require("which-key")
-			vim.api.nvim_set_keymap("", "<leader>gh", "<cmd>Gitsigns preview_hunk<cr>", {noremap = true, desc = "Show hunk"})
-			vim.api.nvim_set_keymap("", "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", {noremap = true, desc = "Reset hunk"})
-			vim.api.nvim_set_keymap("", "<leader>gb", "<cmd>Gitsigns blame_line<cr>", {noremap = true, desc = "Git blame"})
+			vim.api.nvim_set_keymap(
+				"",
+				"<leader>gh",
+				"<cmd>Gitsigns preview_hunk<cr>",
+				{ noremap = true, desc = "Show hunk" }
+			)
+			vim.api.nvim_set_keymap(
+				"",
+				"<leader>gr",
+				"<cmd>Gitsigns reset_hunk<cr>",
+				{ noremap = true, desc = "Reset hunk" }
+			)
+			vim.api.nvim_set_keymap(
+				"",
+				"<leader>gb",
+				"<cmd>Gitsigns blame_line<cr>",
+				{ noremap = true, desc = "Git blame" }
+			)
 		end,
 	},
 }
