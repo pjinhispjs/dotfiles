6dce13cb0495a13b2e09c7afc5d2dbd2cc764dca
pjinhispjs <git@pjmorley.com>
2025-06-23 11:15:54 -0400

    Dragon-drop from nvim

--- .config/nvim/lua/core/keybinds.lua
+++ .config/nvim/lua/core/keybinds.lua
@@ -14,29 +14,40 @@
 --
 
 -- trying space as the leader key
-vim.g.mapleader = ' '
-
+vim.g.mapleader = " "
 
 -- easy window movement
-vim.api.nvim_set_keymap("", "<C-h>", "<C-w>h", {noremap = true, desc = "Select window left"})
-vim.api.nvim_set_keymap("", "<C-j>", "<C-w>j", {noremap = true, desc = "Select window down"})
-vim.api.nvim_set_keymap("", "<C-k>", "<C-w>k", {noremap = true, desc = "Select window up"})
-vim.api.nvim_set_keymap("", "<C-l>", "<C-w>l", {noremap = true, desc = "Select window right"})
+vim.api.nvim_set_keymap("", "<C-h>", "<C-w>h", { noremap = true, desc = "Select window left" })
+vim.api.nvim_set_keymap("", "<C-j>", "<C-w>j", { noremap = true, desc = "Select window down" })
+vim.api.nvim_set_keymap("", "<C-k>", "<C-w>k", { noremap = true, desc = "Select window up" })
+vim.api.nvim_set_keymap("", "<C-l>", "<C-w>l", { noremap = true, desc = "Select window right" })
 
 -- toggle relative line numbers
-vim.api.nvim_set_keymap("n", "<leader>nn", "<cmd>lua vim.opt.number = not(vim.opt.number:get())<cr>", {desc = "Toggle line numbers"})
-vim.api.nvim_set_keymap("n", "<leader>nr", "<cmd>lua vim.opt.relativenumber = not(vim.opt.relativenumber:get())<cr>", {desc = "Toggle relative numbers"})
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>nn",
+	"<cmd>lua vim.opt.number = not(vim.opt.number:get())<cr>",
+	{ desc = "Toggle line numbers" }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>nr",
+	"<cmd>lua vim.opt.relativenumber = not(vim.opt.relativenumber:get())<cr>",
+	{ desc = "Toggle relative numbers" }
+)
 
 -- toggle spelling
-vim.api.nvim_set_keymap("n", "<leader>p", "<cmd>set spell!<cr>", {desc = "Toggle spell checker"})
+vim.api.nvim_set_keymap("n", "<leader>p", "<cmd>set spell!<cr>", { desc = "Toggle spell checker" })
 
 -- clear search
 vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>")
 
 -- yank to system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- dragon-drop the current file
+vim.keymap.set("n", "<leader>fy", "<cmd>! dragon-drop --and-exit %<cr>")
 
 -- Shamelessly stolen from ThePrimeagen --
 -- move lines of text like Sublime's ctrl+shift+arrowkeys
@@ -55,4 +66,4 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- find and replace the word under the cursor
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
---                                      --
+-- End shamelessly stolen from ThePrimeagen --
