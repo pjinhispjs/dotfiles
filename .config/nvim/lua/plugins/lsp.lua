632c6b6c714361c91aa3cdf16c5dc3009da9737b
pjinhispjs <git@pjmorley.com>
2025-07-06 20:34:42 -0400

    Fix broken dependency in mason

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,11 @@
 return {
-	{ 'mason-org/mason.nvim', opts = {} },
-
+	{ "mason-org/mason.nvim", dependencies = { "neovim/nvim-lspconfig" }, opts = {} },
 	{
-	dependencies = { 'neovim/nvim-lspconfig' },
-	opts = {}
+		"hasansujon786/nvim-navbuddy",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"MunifTanjim/nui.nvim",
+		},
+		opts = { lsp = { auto_attach = true } },
 	},
 }
