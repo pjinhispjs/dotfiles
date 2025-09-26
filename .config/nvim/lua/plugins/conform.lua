894b6f2c81afd7eac216c532661fd9914680d958
pjinhispjs <git@pjmorley.com>
2025-09-26 10:40:10 -0400

    Add PHP autoformatting

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,13 @@
 return {
 	"stevearc/conform.nvim",
 	opts = {
+		formatters = {
+			phppsr12 = {
+				command = "php-cs-fixer",
+				stdin = false,
+				args = { "fix", "$FILENAME", "--rules=@PSR12" },
+			},
+		},
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- Conform will run multiple formatters sequentially
@@ -9,6 +16,7 @@ return {
 			-- rust = { "rustfmt", lsp_format = "fallback" },
 			-- Conform will run the first available formatter
 			javascript = { "prettierd", "prettier", stop_after_first = true },
+			php = { "phppsr12" },
 		},
 		-- Set this to change the default values when calling conform.format()
 		-- This will also affect the default values for format_on_save/format_after_save
